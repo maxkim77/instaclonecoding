402fd10250b3c7c1c9aa5a409a6081e6894e7b56
maxkim77 <141907655+maxkim77@users.noreply.github.com>
2024-06-02 20:53:44 +0900

    :sparkles:update

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -2,22 +2,22 @@
 
 namespace App\Http\Middleware;
 
+use Illuminate\Http\Middleware\TrustProxies as Middleware;
 use Illuminate\Http\Request;
-use Fideloper\Proxy\TrustProxies as Middleware;
 
 class TrustProxies extends Middleware
 {
     /**
      * The trusted proxies for this application.
      *
-     * @var array
+     * @var array|string|null
      */
     protected $proxies;
 
     /**
      * The headers that should be used to detect proxies.
      *
-     * @var int
+     * @var string
      */
-    protected $headers = Request::HEADER_X_FORWARDED_ALL;
+    protected $headers = Request::HEADER_X_FORWARDED_FOR;
 }
